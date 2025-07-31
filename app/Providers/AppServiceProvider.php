2ca14b68e0d978d1b4dfdd0ba9600744add3b326
Ghassan Fs <fsghassan2429d@gmail.com>
2025-07-31 17:51:37 +0700

    cleanup appserviceprovider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,8 +3,6 @@
 namespace App\Providers;
 
 use Carbon\Carbon;
-use Filament\Support\Assets\Js;
-use Filament\Support\Facades\FilamentAsset;
 use Filament\Support\Facades\FilamentView;
 use Filament\View\PanelsRenderHook;
 use Illuminate\Support\Facades\Blade;
@@ -28,14 +26,6 @@ class AppServiceProvider extends ServiceProvider
         config(['app.locale' => 'id']);
         Carbon::setLocale('id');
 
-        FilamentAsset::register([
-            Js::make('custom-script', __DIR__.'/../../resources/js/custom.js'),
-        ]);
-
-        FilamentView::registerRenderHook(
-            PanelsRenderHook::HEAD_END,
-            fn (): string => Blade::render('@fluxStyles')
-        );
         FilamentView::registerRenderHook(
             PanelsRenderHook::BODY_END,
             fn (): string => Blade::render('@fluxScripts')
