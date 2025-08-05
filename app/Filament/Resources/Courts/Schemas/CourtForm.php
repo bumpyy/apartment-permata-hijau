3e971aaeed9a974706eb20758c5c87d727b5f0d6
Ghassan Fs <fsghassan2429d@gmail.com>
2025-08-05 11:06:11 +0700

    add court resource

---
+++ app/Filament/Resources/Courts/Schemas/CourtForm.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Filament\Resources\Courts\Schemas;
+
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
+use Filament\Schemas\Schema;
+
+class CourtForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->required(),
+                Textarea::make('description')
+                    ->columnSpanFull(),
+                TextInput::make('hourly_rate')
+                    ->required()
+                    ->numeric()
+                    ->default(0),
+                TextInput::make('light_surcharge')
+                    ->required()
+                    ->numeric()
+                    ->default(50000),
+                Toggle::make('is_active')
+                    ->required(),
+                TextInput::make('operating_hours'),
+            ]);
+    }
+}
